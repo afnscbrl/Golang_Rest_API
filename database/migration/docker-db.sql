9c7e0229852a1376cc83c4498cb6b34c1a857cdb
Afonso Cabral <afns.cbrl@gmail.com>
2022-01-27 19:17:58 -0300

    remodeling the database

--- database/migration/docker-db.sql
+++ database/migration/docker-db.sql
@@ -5,29 +5,33 @@ create table resumes (
     balance real
 );
 
+create table outcomes_cat (
+	id integer primary key,
+	category varchar NOT NULL UNIQUE
+);
+
 create table incomes (
     id serial primary key,
-    describe varchar NOT NULL check (describe != ''),
+    describe varchar(255) NOT NULL check (describe != ''),
     value real NOT NULL check (value > 0),
-    date varchar NOT NULL check (date != ''),
-  	month integer not null default (extract(MONTH FROM CURRENT_DATE)),
+    date varchar(11) NOT NULL check (date != ''),
+    day integer NOT NULL,
+    month integer NOT NULL,
+    year integer NOT NULL,
   	constraint duplicate_income unique (describe, month)
 );
 
 create table outcomes (
     id serial primary key,
-    describe varchar NOT NULL check (describe != ''),
+    describe varchar(255) NOT NULL check (describe != ''),
     value real NOT NULL check (value > 0),
-    date varchar NOT NULL check (date != ''),
-	category varchar NOT NULL check (category != '') references outcomes_cat(category), 
-    month integer NOT NULL default (extract(MONTH FROM CURRENT_DATE)),
+    date varchar(11) NOT NULL check (date != ''),
+	category varchar(25) NOT NULL check (category != '') references outcomes_cat(category), 
+    day integer NOT NULL,
+    month integer NOT NULL,
+    year integer NOT NULL,
     constraint duplicate_outcome unique (describe, month)
 );
-
-create table outcomes_cat (
-	id integer primary key,
-	category varchar NOT NULL UNIQUE
-)
 
 insert into outcomes_cat (id, category) values 
 	(1, 'Alimentação'),

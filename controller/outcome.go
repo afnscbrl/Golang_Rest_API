730a889ff640f231b71f4648868528fd5098cc5d
Afonso Cabral <afns.cbrl@gmail.com>
2022-01-27 19:20:49 -0300

    controller splited and add new rules

---
+++ controller/outcome.go
@@ -0,0 +1,116 @@
+package controller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/afnscbrl/Golang_Rest_API/database"
+	"github.com/afnscbrl/Golang_Rest_API/models"
+	"github.com/gorilla/mux"
+)
+
+//This controller contains all func that control the in/out data from requests and db
+
+// func Home(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Home Page")
+// }
+
+// func Dashboard(w http.ResponseWriter, r *http.Request) {
+// 	var res []models.Resume
+// 	// var inc []models.Income
+// 	// var outp []models.OutcomePer
+// 	// var outv []models.OutcomeVar
+// 	// var bal []models.Balance
+// 	database.DB.Find(&res)
+// 	json.NewEncoder(w).Encode(res)
+// }
+
+func NewOutcome(w http.ResponseWriter, r *http.Request) {
+	var newOutcome models.Outcome
+	json.NewDecoder(r.Body).Decode(&newOutcome)
+	if newOutcome.Category == "" {
+		newOutcome.Category = "Outras"
+	}
+	dates := strings.Split(newOutcome.Date, "-")
+	day, _ := strconv.Atoi(dates[2])
+	month, _ := strconv.Atoi(dates[1])
+	year, _ := strconv.Atoi(dates[0])
+	if (day < 1 || day > 31) || (month < 1 || month > 12) {
+		log.Println("Error 400: BAD RESQUEST - Wrong data type")
+		w.WriteHeader(400)
+		return
+	}
+	newOutcome.Year = year
+	newOutcome.Month = month
+	newOutcome.Day = day
+	database.DB.Create(&newOutcome)
+	json.NewEncoder(w).Encode(newOutcome)
+}
+
+func Outcome(w http.ResponseWriter, r *http.Request) {
+
+	v := r.URL.Query()
+	if !v.Has("describe") {
+		var out []models.Outcome
+		database.DB.Find(&out)
+		json.NewEncoder(w).Encode(out)
+
+	} else {
+		desc := v.Get("describe")
+		var outcome models.Outcome
+		database.DB.Where("describe = ?", desc).First(&outcome)
+		json.NewEncoder(w).Encode(outcome)
+	}
+}
+
+func OutcomeDetail(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var outcome models.Outcome
+	database.DB.First(&outcome, id)
+	json.NewEncoder(w).Encode(outcome)
+
+}
+
+func EditOutcome(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var outcome models.Outcome
+	database.DB.First(&outcome, id)
+	json.NewDecoder(r.Body).Decode(&outcome)
+	dates := strings.Split(outcome.Date, "-")
+	day, _ := strconv.Atoi(dates[2])
+	month, _ := strconv.Atoi(dates[1])
+	year, _ := strconv.Atoi(dates[0])
+	if (day < 1 || day > 31) || (month < 1 || month > 12) {
+		log.Println("Error 400: BAD RESQUEST - Wrong data type")
+		w.WriteHeader(400)
+		return
+	}
+	outcome.Year = year
+	outcome.Month = month
+	outcome.Day = day
+	database.DB.Save(&outcome)
+	json.NewEncoder(w).Encode(outcome)
+}
+
+func DeleteOutcome(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var outcome models.Outcome
+	database.DB.Delete(&outcome, id)
+	json.NewEncoder(w).Encode(outcome)
+}
+
+func OutcomeByMonth(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	year := vars["year"]
+	month := vars["month"]
+
+	var outcome []models.Outcome
+	database.DB.Where("year = ? AND month = ?", year, month).Find(&outcome)
+	json.NewEncoder(w).Encode(outcome)
+}

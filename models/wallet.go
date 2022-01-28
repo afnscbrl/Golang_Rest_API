e23ac6e589ce25de6ef0d92f9f0a04f6f5dd1784
Afonso Cabral <afns.cbrl@gmail.com>
2022-01-27 22:03:42 -0300

    balance struct added

--- models/wallet.go
+++ models/wallet.go
@@ -1,7 +1,6 @@
 package models
 
 type Resume struct {
-	Id           int     `json:"id"`
 	TotalIncome  float64 `json:"total income"`
 	TotalOutcome float64 `json:"total outcome"`
 	Balance      float64 `json:"balance"`

c5a367a800512b3dceefdf257e253d8a8c022ffc
afnscbrl <afns.cbrl@gmail.com>
2022-02-06 00:02:07 -0300

    added auth route

--- routes/routes.go
+++ routes/routes.go
@@ -14,24 +14,27 @@ import (
 //This function contains all URL routes of API and the html methods.
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.HandleFunc("/login", controller.Login).Methods("Post")
 	//Quando implementar o front, editar o Middleware
-	r.Use(middleware.ContentTypeMiddleware)
 
+	subRouter := r.PathPrefix("/api/").Subrouter()
+	subRouter.Use(middleware.ContentTypeMiddleware)
 	// r.HandleFunc("/", controller.Home)
 	// r.HandleFunc("/api/dashboard", controller.Dashboard).Methods("Get")
-	r.HandleFunc("/api/resumo/{year}/{month}", controller.BalanceByMonth).Methods("Get")
-	r.HandleFunc("/api/receitas", controller.NewIncome).Methods("Post")
-	r.HandleFunc("/api/receitas", controller.Income).Methods("Get")
-	r.HandleFunc("/api/receitas/{id}", controller.IncomeDetail).Methods("Get")
-	r.HandleFunc("/api/receitas/{id}", controller.EditIncome).Methods("Put")
-	r.HandleFunc("/api/receitas/{id}", controller.DeleteIncome).Methods("Delete")
-	r.HandleFunc("/api/receitas/{year}/{month}", controller.IncomeByMonth).Methods("Get")
-	r.HandleFunc("/api/despesas", controller.NewOutcome).Methods("Post")
-	r.HandleFunc("/api/despesas", controller.Outcome).Methods("Get")
-	r.HandleFunc("/api/despesas/{id}", controller.OutcomeDetail).Methods("Get")
-	r.HandleFunc("/api/despesas/{id}", controller.EditOutcome).Methods("Put")
-	r.HandleFunc("/api/despesas/{id}", controller.DeleteOutcome).Methods("Delete")
-	r.HandleFunc("/api/despesas/{year}/{month}", controller.OutcomeByMonth).Methods("Get")
+	subRouter.HandleFunc("/resumo/{year}/{month}", controller.BalanceByMonth).Methods("Get")
+	subRouter.HandleFunc("/receitas", controller.NewIncome).Methods("Post")
+	subRouter.HandleFunc("/receitas", controller.Income).Methods("Get")
+	subRouter.HandleFunc("/receitas/{id}", controller.IncomeDetail).Methods("Get")
+	subRouter.HandleFunc("/receitas/{id}", controller.EditIncome).Methods("Put")
+	subRouter.HandleFunc("/receitas/{id}", controller.DeleteIncome).Methods("Delete")
+	subRouter.HandleFunc("/receitas/{year}/{month}", controller.IncomeByMonth).Methods("Get")
+	subRouter.HandleFunc("/despesas", controller.NewOutcome).Methods("Post")
+	subRouter.HandleFunc("/despesas", controller.Outcome).Methods("Get")
+	subRouter.HandleFunc("/despesas/{id}", controller.OutcomeDetail).Methods("Get")
+	subRouter.HandleFunc("/despesas/{id}", controller.EditOutcome).Methods("Put")
+	subRouter.HandleFunc("/despesas/{id}", controller.DeleteOutcome).Methods("Delete")
+	subRouter.HandleFunc("/despesas/{year}/{month}", controller.OutcomeByMonth).Methods("Get")
+
 	//set port 8000 to list and serve
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }

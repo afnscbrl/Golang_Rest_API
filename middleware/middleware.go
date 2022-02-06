f9648530494d759deb60130b88ca858629a133c6
afnscbrl <afns.cbrl@gmail.com>
2022-02-06 00:01:21 -0300

    added auth verification

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,10 +1,49 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/afnscbrl/Golang_Rest_API/controller"
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtKey = []byte("secret_key")
 
 func ContentTypeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-type", "application/json")
+		cookie, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		tokenStr := cookie.Value
+
+		claims := &controller.Claims{}
+
+		tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+			func(t *jwt.Token) (interface{}, error) {
+				return jwtKey, nil
+			})
+
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if !tkn.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }

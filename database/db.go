5a59235dc6ef2fb5620390778236be2f47717ac9
afnscbrl <afns.cbrl@gmail.com>
2022-02-09 18:08:39 -0300

    modified to connect db in heroku

--- database/db.go
+++ database/db.go
@@ -22,12 +22,12 @@ func ConnectWithDB() {
 		fmt.Print(e)
 	}
 	//
-	DB_HOST := os.Getenv("DATABASE_HOST")
-	//DB_PASS := os.Getenv("DATABASE_PASS")
-	//DB_URL := os.Getenv("DATABASE_URL")
-	//DB_USER := os.Getenv("DATABASE_USER")
-	//DB_DB := os.Getenv("DATABASE_DB")
-	//connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", DB_URL, DB_USER, DB_PASS, DB_DB)
+	DB_HOST := os.Getenv("DATABASE_URL")
+	// DB_PASS := os.Getenv("DATABASE_PASS")
+	// DB_URL := os.Getenv("DATABASE_HOST")
+	// DB_USER := os.Getenv("DATABASE_USER")
+	// DB_DB := os.Getenv("DATABASE_DB")
+	// connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", DB_URL, DB_USER, DB_PASS, DB_DB)
 	DB, err = gorm.Open(postgres.Open(DB_HOST))
 	if err != nil {
 		log.Panic("ERROR - Connection with db fail")
